d6d8994cff88ef13a198e567daca120567a3f101
Ratul Saha <50.ratulsaha@gmail.com>
2017-03-29 15:09:55 +0530

    Updated to include new functions

--- MsharP3/Program.fs
+++ MsharP3/Program.fs
@@ -4,13 +4,30 @@ open System
 open Header
 open Sideinfo
 open Maindata
+open Frame
+open MathUtils
 
 module Test = 
     //Test Data
     let main = 
         let data = IO.File.ReadAllBytes "aframe.mp3"
-        let header = data.[0..3] |> parseHeader
-        let sideconfig = header |> parseSideConfig data.[4..35]
-        sideconfig |> printfn "%A"
-        parseMainData data.[36..] header sideconfig |> printfn "%A"
+        
+        let result = 
+            for i = 0 to 1000 do
+                //Get Frame header
+                let header = data.[0..3] |> parseHeader
+        
+                //Get Frame Sideinfo
+                let sideconfig = header |> parseSideConfig data.[4..35]
+                
+                //Get Frameinfo
+                let frameinfo = getFrameInfo header
+
+                //Get Maindata
+                let (scalefactors,samples) = parseMainData data.[36..] header frameinfo sideconfig
+                printfn "%d\r" i
+
+                //Requantize
+                let result = requantizeSamples sideconfig.sideInfoGr.[0] frameinfo scalefactors.[0] samples.[0]
+                result |> ignore
         System.Console.ReadLine();
