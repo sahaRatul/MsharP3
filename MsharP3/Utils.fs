3093b1ac4a2d623719b94a13bfc2ce7b60260fa8
RatulSaha <50.ratulsaha@gmail.com>
2017-04-01 18:59:36 +0530

    Modified to avoid typecasts

--- MsharP3/Utils.fs
+++ MsharP3/Utils.fs
@@ -4,30 +4,30 @@ module Utils =
     //Get a Array of bits from a Array of bytes
     let getBitsArrayfromByteArray x = 
         let getBits x = 
-            let y = [|0;0;0;0;0;0;0;0|]
-            y.[7] <- (x >>> 0 &&& 0x01)
-            y.[6] <- (x >>> 1 &&& 0x01)
-            y.[5] <- (x >>> 2 &&& 0x01)
-            y.[4] <- (x >>> 3 &&& 0x01)
-            y.[3] <- (x >>> 4 &&& 0x01)
-            y.[2] <- (x >>> 5 &&& 0x01)
-            y.[1] <- (x >>> 6 &&& 0x01)
-            y.[0] <- (x >>> 7 &&& 0x01)
+            let y = [|0uy;0uy;0uy;0uy;0uy;0uy;0uy;0uy|]
+            y.[7] <- (x >>> 0 &&& 0x01uy)
+            y.[6] <- (x >>> 1 &&& 0x01uy)
+            y.[5] <- (x >>> 2 &&& 0x01uy)
+            y.[4] <- (x >>> 3 &&& 0x01uy)
+            y.[3] <- (x >>> 4 &&& 0x01uy)
+            y.[2] <- (x >>> 5 &&& 0x01uy)
+            y.[1] <- (x >>> 6 &&& 0x01uy)
+            y.[0] <- (x >>> 7 &&& 0x01uy)
             y
         x |> Array.map getBits |> Array.concat
 
     //Generate Number from array of bits (x<32 bits
-    let bitsArraytoNumber (x:array<int>) = 
+    let bitsArraytoNumber (x:array<byte>) = 
         let mutable out = 0
         let len = x.Length
         for i = 0 to (len-1) do
-            out <- (out <<< 1) ||| x.[i]
+            out <- (out <<< 1) ||| (x.[i] |> int)
         out
 
     //Extract bits from a bits array and convert to number
     let getBits count (x:array<byte>) = 
         let temp = (x.[0..(count-1)])
-        let ret = (temp |> Array.map int |> bitsArraytoNumber,x.[count..])
+        let ret = (temp |> bitsArraytoNumber,x.[count..])
         ret
 
     //For 32 bit
@@ -44,6 +44,5 @@ module Utils =
 
     let getBits2 start count (x:array<byte>) = 
         let result = 
-            x.[start..(start + count - 1)]
-            |> Array.map int |> bitsArraytoNumber
+            x.[start..(start + count - 1)] |> bitsArraytoNumber
         (result,start+count)
