c3983e21cf7bd000dac84c915f9ed12123fb0a7e
RatulSaha <50.ratulsaha@gmail.com>
2017-04-01 19:01:25 +0530

    Can sort handle multiple mp3 frames now

--- MsharP3/Program.fs
+++ MsharP3/Program.fs
@@ -9,45 +9,51 @@ open MathUtils
 
 module Test = 
     //Test Data
-
     let main = 
-        let data = IO.File.ReadAllBytes "aframe.mp3"
-        
-        let result = 
-            for i = 0 to 1000 do
-                //Get Frame header
-                let header = data.[0..3] |> parseHeader
-        
-                //Get Frame Sideinfo
-                let sideconfig = header |> parseSideConfig data.[4..35]
-                
-                //Get Frameinfo
-                let frameinfo = getFrameInfo header
+        let data = IO.File.ReadAllBytes "Again.mp3"
+        let mutable offset = 0
+        let mutable count = 0
+        while offset < data.Length do
+            //Get Frame header
+            let header = data.[offset..(3 + offset)] |> parseHeader
+            offset <- offset + 4
 
-                //Get Maindata
-                let (scalefactors,samples) = parseMainData data.[36..] header frameinfo sideconfig
+            //Get Frame Sideinfo
+            let sideinfosize = if header.channelMode = 3uy then 16 else 31
+            let sideconfig = header |> parseSideConfig data.[offset..(sideinfosize + offset)]
+            offset <- offset + sideinfosize + 1
 
-                //Requantize
-                let requantizedSamples = Array.map3 (requantizeSamples frameinfo) sideconfig.sideInfoGr scalefactors samples
+            //Get Frameinfo
+            let frameinfo = getFrameInfo header
+
+            //Get Maindata
+            let (scalefactors,samples) = parseMainData data.[offset..(offset + frameinfo.frameSize - 1 - sideinfosize)] header frameinfo sideconfig
+            offset <- offset + frameinfo.frameSize - 4 - sideinfosize - 1
+
+            //Requantize
+            let requantizedSamples = Array.map3 (requantizeSamples frameinfo) sideconfig.sideInfoGr scalefactors samples
                 
-                //Mid side 
-                let result = 
-                    if header.channelMode = 1uy
-                        then
-                            let samplesgranule0 = decodeMidSide requantizedSamples.[0] requantizedSamples.[1]
-                            let samplesgranule1 = decodeMidSide requantizedSamples.[2] requantizedSamples.[3]
-                            Array.concat [|samplesgranule0;samplesgranule1|]
+            //Mid side 
+            let result = 
+                if header.channelMode = 1uy
+                    then
+                        let samplesgranule0 = decodeMidSide requantizedSamples.[0] requantizedSamples.[1]
+                        let samplesgranule1 = decodeMidSide requantizedSamples.[2] requantizedSamples.[3]
+                        Array.concat [|samplesgranule0;samplesgranule1|]
+                    else
+                        requantizedSamples
+                
+            //Reorder/Alias reduction
+            let result2 = Array.zeroCreate 4
+            for i = 0 to (if header.channelMode = 3uy then 1 else 3) do
+                result2.[i] <-
+                    if (sideconfig.sideInfoGr.[i].mixedBlockFlag || sideconfig.sideInfoGr.[i].blockType = 2) 
+                        then reorderSamples frameinfo result.[i]
                         else
-                            requantizedSamples
-                
-                //Reorder/Alias reduction
-                let result2 = Array.zeroCreate 4
-                for i = 0 to (if header.channelMode = 3uy then 1 else 3) do
-                    result2.[i] <-
-                        if (sideconfig.sideInfoGr.[i].mixedBlockFlag || sideconfig.sideInfoGr.[i].blockType = 2) 
-                            then reorderSamples frameinfo result.[i]
-                            else
-                                reduceAlias sideconfig.sideInfoGr.[i] result.[i]
-                printfn "%d" i
-                result2 |> ignore
-        result
+                            reduceAlias sideconfig.sideInfoGr.[i] result.[i]
+            
+            //IMDCT
+            let result3 = Array.map2 (fun x y -> IMDCT x y) sideconfig.sideInfoGr result2
+            printfn "Count = %d" count
+            count <- count + 1
+            result2 |> ignore
